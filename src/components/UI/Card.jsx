f2afbae2ad8ae54900b60ee7fff109d392478e42
Muhammad Aqib Javed <91584862+AqibNiazi@users.noreply.github.com>
2023-09-27 23:35:56 +0500

    first commit

--- src/components/UI/Card.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./Card.module.css";
-const Card = ({ children, className }) => {
-  const classes = `${styles.card} ${className}`;
-  return <div className={classes}>{children}</div>;
-};
-
-export default Card;
